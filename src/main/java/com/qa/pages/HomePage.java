6370441a77ab17b3942e484c538ee030c86d10a9
mrayapur <mohanalakshmi167@gmail.com>
2021-06-05 19:02:05 +0530

    updated signin and homepage

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -5,5 +5,9 @@ public class HomePage {
 	public void homePage() {
 		System.out.println("Home Page");
 	}
+	
+	public void homeSet() {
+		System.out.println("Home Setup");
+	}
 
 }
---
+++ src/main/java/com/qa/pages/SignInPage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class SignInPage {
+	public void signInPage() {
+		System.out.println("SignIn Page");
+	}
+
+}
